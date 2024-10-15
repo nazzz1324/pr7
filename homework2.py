b9765a7d457e2312298431f32f428fa4a54ff64f
nazzz1324 <ovovkopo@gmail.com>
2024-10-15 21:51:42 +0500

    Слетел gitignore и LICENSE

--- homework2.py
+++ homework2.py
@@ -3,6 +3,6 @@ number = input("Введите целое простое десятичное ч
 if number.isdigit():
     number = int(number)
     print(f"Двоичное представление: {bin(number)[2:]}")
-    print(f"Восьмеричное представление: {oct(number)[2:]}")
+    print(f"Восьмеричное пред ставление: {oct(number)[2:]}")
 else:
     print("Ошибка: введите корректное целое число.")
